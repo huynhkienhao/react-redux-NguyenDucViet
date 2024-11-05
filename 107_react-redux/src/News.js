80be29c69f18275785c4eaf1b30ee3abe1c4edcf
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-11-06 02:06:47 +0700

    111

--- 107_react-redux/src/News.js
+++ 107_react-redux/src/News.js
@@ -3,15 +3,16 @@ import { connect } from 'react-redux';
 
 class News extends Component {
 
-    useEditStatusInStore = () => {
-        let { dispatch } = this.props;
-        dispatch({ type: 'CHANGE_EDIT_STATUS' })
-    }
+    // useEditStatusInStore = () => {
+    //     let { dispatch } = this.props;
+    //     dispatch({ type: 'CHANGE_EDIT_STATUS' })
+    // }
+
     render() {
         return (
             <div>
                 <h2>Đây là component News</h2>
-                <button onClick={() => this.useEditStatusInStore()}>Click here</button>
+                <button onClick={() => this.props.useEditStatusInStore()}>Click here</button>
             </div>
         );
     }
@@ -23,4 +24,12 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps)(News);
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        useEditStatusInStore: () => {
+            dispatch({ type: 'CHANGE_EDIT_STATUS' })
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(News);
