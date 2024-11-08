01ebd08057a9d324aa9892be04247a925d8fc719
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-11-09 01:23:12 +0700

    120

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { firebaseConnect } from './firebaseConnect';
-import { getDatabase, ref, push } from "firebase/database";
+import { getDatabase, ref, push, remove } from "firebase/database";
 
 class App extends Component {
 
+  // Thêm dữ liệu
   pushData = () => {
     const database = getDatabase(firebaseConnect);
     const connectData = ref(database, 'dataForNote');
@@ -14,6 +15,16 @@ class App extends Component {
       content: 'Nội dung ghi chú số 3'
     }).then(() => {
       console.log('Thêm dữ liệu thành công');
+    })
+  }
+
+  // Xóa dữ liệu
+  deleteData = (id) => {
+    const database = getDatabase(firebaseConnect);
+    const connectData = ref(database, `dataForNote/${id}`);
+
+    remove(connectData).then(() => {
+      console.log(`Xóa dữ liệu thành công có id là ${id}`);
     })
   }
 
@@ -26,6 +37,8 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <button onClick={() => this.pushData()}>Click vào để thêm mới với hàm push</button>
+          <br />
+          <button onClick={() => this.deleteData('-OBC-E25ASYaY3Xt0k6n')}>Click vào để xóa dữ liệu</button>
         </header>
       </div>
     );
