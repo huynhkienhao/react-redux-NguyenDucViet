7fe12bba39aa3cbe051cbfb5cff25d45315f7a6b
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-11-11 18:26:46 +0700

    video 122

--- 114_note_manager/src/components/NoteForm.js
+++ 114_note_manager/src/components/NoteForm.js
@@ -1,42 +1,69 @@
 import React, { Component } from 'react';
 
 class NoteForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            noteTitle: '',
+            noteContent: ''
+        }
+    }
+
+    isChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        console.log(name);
+        console.log(value);
+
+        this.setState({
+            [name]: value
+        });
+
+    }
+
+    addData = (title, content) => {
+        let item = {};
+
+        item.title = title;
+        item.content = content;
+
+        console.log(item);
+
+    }
     render() {
         return (
             <div className="col-4">
                 <h3>SỬA NỘI DUNG NOTE</h3>
-                <div className="form-group">
-                    <label htmlFor="noteTitle">Tiêu đề note</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        name="noteTitle"
-                        id="noteTitle"
-                        aria-describedby="helpIdNoteTitle"
-                        placeholder="Tiêu đề note"
-                    />
-                    <small id="helpIdNoteTitle" className="form-text text-muted">
-                        Điền tiêu đề vào đây
-                    </small>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="noteContent">Nội dung note</label>
-                    <textarea
-                        type="text"
-                        className="form-control"
-                        name="noteContent"
-                        id="noteContent"
-                        aria-describedby="helpIdNoteContent"
-                        placeholder="Nội dung note"
-                        defaultValue={""}
-                    />
-                    <small id="helpIdNoteContent" className="form-text text-muted">
-                        Điền nội dung vào đây
-                    </small>
-                </div>
-                <button type="submit" className="btn btn-primary">
-                    Lưu
-                </button>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="noteTitle">Tiêu đề note</label>
+                        <input type="text"
+                            className="form-control"
+                            name="noteTitle"
+                            id="noteTitle"
+                            aria-describedby="helpIdNoteTitle"
+                            placeholder="Tiêu đề note"
+                            onChange={(event) => this.isChange(event)} />
+                        <small id="helpIdNoteTitle" className="form-text text-muted">Điền tiêu đề vào đây</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="noteContent">Nội dung note</label>
+                        <textarea
+                            type="text"
+                            className="form-control"
+                            name="noteContent"
+                            id="noteContent"
+                            aria-describedby="helpIdNoteContent"
+                            placeholder="Nội dung note"
+                            defaultValue={""}
+                            onChange={(event) => this.isChange(event)} />
+                        <small id="helpIdNoteContent" className="form-text text-muted">Điền nội dung vào đây</small>
+                    </div>
+                    <button type="reset" onClick={() => this.addData(this.state.noteTitle, this.state.noteContent)} className="btn btn-primary">
+                        Lưu
+                    </button>
+                </form>
             </div>
         );
     }
